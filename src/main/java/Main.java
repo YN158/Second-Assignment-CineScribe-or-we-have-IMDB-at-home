dbc3582281b2f1dd5456798c887a00987153abb6
YasharNasiri <bluemustarduniversity@gmail.com>
2024-03-01 10:53:37 +0330

    Set up for graphical interface

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,46 @@
+import java.awt.Canvas;
+import java.awt.Graphics;
+import javax.swing.JFrame;
 public class Main
 {
+
+    static Boolean Running;
+
     public static void main(String[] args)
     {
-        // TODO --> complete main function
-        runMenu();
+        JFrame frame = new JFrame("My Drawing");
+        Canvas canvas = new Canvas();
+        canvas.setSize(800, 500);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
+        AppStart();
+        AppLoop();
     }
-    public static void runMenu()
+    public static void AppStart()
     {
-        // TODO
+
+    }
+    public static void AppLoop()
+    {
+        while (true)
+        {
+            Input();
+            LogicUpdate();
+            GraphicsUpdate();
+        }
+    }
+    public static void Input()
+    {
+
+    }
+    public static void LogicUpdate()
+    {
+
+    }
+
+    public static void GraphicsUpdate()
+    {
+
     }
 }
